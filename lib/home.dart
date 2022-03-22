9dc64aa8fc2738ff57e2c526a3b59fae82564c15
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2022-03-22 20:01:41 +0500

    Main__Frontend__AI_Buttons_Visible

--- lib/home.dart
+++ lib/home.dart
@@ -87,142 +87,128 @@ class _HomeState extends State<Home> {
               colors: [Color(0xFFa8e063), Color(0xFF56ab2f)]),
         ),
         child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    height: 50,
-                  ),
-                  Text(
-                    'Detect Flowers',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w800,
-                        fontSize: 28),
-                  ),
-                  Text(
-                    'Custom Tensorflow CNN',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18),
-                  ),
-                  SizedBox(height: 40),
-                  Container(
-                      padding: EdgeInsets.all(30),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(30),
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.black.withOpacity(0.5),
-                              spreadRadius: 5,
-                              blurRadius: 7),
-                        ],
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Detect Flowers',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w800,
+                    fontSize: 28),
+              ),
+              Text(
+                'Custom Tensorflow CNN',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18),
+              ),
+              SizedBox(height: 40),
+              Container(
+                padding: EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7),
+                  ],
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: Center(
+                        child: _loading
+                            ? Container(
+                                width: 300,
+                                child: Column(
+                                  children: <Widget>[
+                                    Image.asset('assets/flower.png'),
+                                    SizedBox(height: 40),
+                                  ],
+                                ),
+                              )
+                            : Container(
+                                child: Column(
+                                  children: <Widget>[
+                                    Container(
+                                      height: 300,
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(10),
+                                        child: Image.file(_image),
+                                      ),
+                                    ),
+                                    SizedBox(height: 20),
+                                    _output != null
+                                        ? Text(
+                                            'Prediction is: ${_output[0]['label']}',
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 20))
+                                        : Container(),
+                                    SizedBox(height: 30),
+                                  ],
+                                ),
+                              ),
                       ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
                       child: Column(
                         children: <Widget>[
-                          Container(
-                            child: Center(
-                                child: _loading
-                                    ? Container(
-                                        width: 300,
-                                        child: Column(
-                                          children: <Widget>[
-                                            Image.asset('assets/flower.png'),
-                                            SizedBox(height: 40),
-                                          ],
-                                        ),
-                                      )
-                                    : Container(
-                                        child: Column(
-                                        children: <Widget>[
-                                          Container(
-                                            height: 300,
-                                            child: ClipRRect(
-                                              borderRadius:
-                                                  BorderRadius.circular(10),
-                                              child: Image.file(_image),
-                                            ),
-                                          ),
-                                          SizedBox(height: 20),
-                                          _output != null
-                                              ? Text(
-                                                  'Prediction is: ${_output[0]['label']}',
-                                                  style: TextStyle(
-                                                      color: Colors.black,
-                                                      fontSize: 20))
-                                              : Container(),
-                                          SizedBox(height: 30),
-                                          Container(
-                                              width: MediaQuery.of(context)
-                                                  .size
-                                                  .width,
-                                              child: Column(
-                                                children: <Widget>[
-                                                  GestureDetector(
-                                                    onTap: pickImage,
-                                                    child: Container(
-                                                      width:
-                                                          MediaQuery.of(context)
-                                                                  .size
-                                                                  .width -
-                                                              180,
-                                                      alignment:
-                                                          Alignment.center,
-                                                      padding:
-                                                          EdgeInsets.symmetric(
-                                                              horizontal: 24,
-                                                              vertical: 17),
-                                                      decoration: BoxDecoration(
-                                                          color:
-                                                              Color(0xFF56ab2f),
-                                                          borderRadius:
-                                                              BorderRadius
-                                                                  .circular(6)),
-                                                      child: Text(
-                                                        'Take a photo',
-                                                        style: TextStyle(
-                                                            color: Colors.white,
-                                                            fontSize: 18),
-                                                      ),
-                                                    ),
-                                                  ),
-                                                  SizedBox(height: 5),
-                                                  GestureDetector(
-                                                    onTap: pickImage,
-                                                    child: Container(
-                                                      width:
-                                                          MediaQuery.of(context).size.width -180,
-                                                      alignment:
-                                                          Alignment.center,
-                                                      padding:
-                                                          EdgeInsets.symmetric(
-                                                              horizontal: 24,
-                                                              vertical: 17),
-                                                      decoration: BoxDecoration(
-                                                          color:
-                                                              Color(0xFF56ab2f),
-                                                          borderRadius:
-                                                              BorderRadius
-                                                                  .circular(6)),
-                                                      child: Text(
-                                                        'Camera Roll',
-                                                        style: TextStyle(
-                                                            color: Colors.white,
-                                                            fontSize: 18),
-                                                      ),
-                                                    ),
-                                                  ),
-                                                ],
-                                              ))
-                                        ],
-                                      ))),
-                          )
+                          GestureDetector(
+                            onTap: pickImage,
+                            child: Container(
+                              width: MediaQuery.of(context).size.width - 180,
+                              alignment: Alignment.center,
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 24, vertical: 17),
+                              decoration: BoxDecoration(
+                                color: Color(0xFF56ab2f),
+                                borderRadius: BorderRadius.circular(6),
+                              ),
+                              child: Text(
+                                'Take a photo',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 18),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          GestureDetector(
+                            onTap: pickGalleryImage,
+                            child: Container(
+                              width: MediaQuery.of(context).size.width - 180,
+                              alignment: Alignment.center,
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 24, vertical: 17),
+                              decoration: BoxDecoration(
+                                color: Color(0xFF56ab2f),
+                                borderRadius: BorderRadius.circular(6),
+                              ),
+                              child: Text(
+                                'Camera Roll',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 18),
+                              ),
+                            ),
+                          ),
                         ],
-                      ))
-                ])),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
